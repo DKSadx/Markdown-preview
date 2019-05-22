c072c218d2ad1cf18b7107f18bc39471e95c94ed
DKSadx <sadx.carmon@gmail.com>
2019-05-22 17:29:06 +0000

    Table and blockquotes finished

--- src/Components/CodeBlock/CodeBlock.js
+++ src/Components/CodeBlock/CodeBlock.js
@@ -4,6 +4,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from "prop-types";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// theme duetoneDark
 import { duotoneDark } from "react-syntax-highlighter/dist/styles/prism";
 
 export class CodeBlock extends PureComponent {
